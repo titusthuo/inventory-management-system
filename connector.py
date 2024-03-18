35c1902fa93cac6e085baf12c926b56a8cf1852b
titusthuo <121286467+titusthuo@users.noreply.github.com>
2024-03-18 23:21:13 +0300

    Update connector.py

--- connector.py
+++ connector.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="107220Mwangi@",
+    password="your_password",
     database="inventory_management_system"
 )
 
